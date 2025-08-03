173f6d6717a13e5ecaccc5bcf6f1486f583476de
Farhan Yousafzai <coderwithferry@gmail.com>
2025-08-03 15:15:13 +0500

    Nav

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,9 +1,11 @@
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({ children }) {
+  const isLoggedIn = {firstName: 'John', lastName: 'Doe'}
   return (
     
-      <main>
-SIDEBAR
+      <main className="w-full h-screen flex">
+        <Sidebar user={isLoggedIn} />
 {children}
       </main>
       
